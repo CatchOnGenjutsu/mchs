8573e52ff30791fb7d081a2775dd782c48f367e5
CatchOnGenjutsu <kozlov.dmitriy.dev@gmail.com>
2023-01-18 17:58:57 +0300

    search component added, structure and styles redefined in several components

--- src/components/DataBase.js
+++ src/components/DataBase.js
@@ -1,12 +1,41 @@
 import React from "react";
 import Header from "./Header.js";
 import Sidebar from "./Sidebar.js";
+import SearchBlock from "./SearchBlock.js";
+import "../styles/DataBase.css"
 
 function DataBase() {
+  const inputsHeaders = [
+    {
+      key: "firstname",
+      value: "Имя"
+    },
+    {
+      key: "secondname",
+      value: "Фамилия"
+    },
+    {
+      key: "lastname",
+      value: "Отчество"
+    },
+    {
+      key: "govnumber",
+      value: "Гос. номер",
+      description: "В формате 1111 XX-1"
+    }
+  ]
   return (
     <>
       <Header showButton={true} />
-      <Sidebar />
+      <div className="page-block">
+        <Sidebar />
+        <div className="content-block">
+          <h2>База данных маломерных судов</h2>
+          <SearchBlock inputsHeaders={inputsHeaders} />
+        </div>
+
+      </div>
+
     </>
 
   )
---
+++ src/components/SearchBlock.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import "../styles/SearchBlock.css"
+
+
+export default function SearchBlock(props) {
+  return (
+    <>
+      <Form className="form-inputs">
+        <div className="area-inputs">
+          {props.inputsHeaders.map((item) => {
+            return (
+              <Form.Group key={item.key} className="input-element" controlId="formBasicEmail">
+                <Form.Label className="label-text">{item.value}</Form.Label>
+                <Form.Control className="entry-field" type="text" />
+                {item.description !== undefined ?
+                  <Form.Text className="description-text">
+                    {item.description}
+                  </Form.Text> : null
+                }
+              </Form.Group>
+            )
+          })}
+        </div>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
+
+  )
+}
+
