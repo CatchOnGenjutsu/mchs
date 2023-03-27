947fab6d82a75759305a536c5cc3a58ac0e1b9f8
CatchOnGenjutsu <kozlov.dmitriy.dev@gmail.com>
2023-03-27 11:31:35 +0300

    added validation on empty inputs in certificate modal window component

--- src/components/Certificate/ModalWindow/CertificateModalWindow.jsx
+++ src/components/Certificate/ModalWindow/CertificateModalWindow.jsx
@@ -20,12 +20,15 @@ export default function CertificateModalWindow({
     return dictionaryReducer.usersLibrary;
   });
 
-  const [newMark, setNewMark] = useState(
+  const [newData, setNewData] = useState(
     type === "edit"
     ? structuredClone(dataForEdit)
     : { userid: usersLib[0] }
   );
+  const [errors, setErrors]= useState({});
+  const [saveKey, setSaveKey] = useState(false);
 
+  const errorsFields = modalWindowInputs.nameColumn.map(item => Object.values(item)).map(elem => elem[2]);
 
   // useEffect(() => {
   // }, []);
@@ -33,176 +36,217 @@ export default function CertificateModalWindow({
   const handleChange = (e) => {
     switch (modalWindowInputs.keyTable) {
       case 'lossControl':
-        newMark[e.currentTarget.dataset.id] = e.currentTarget.value;
-        newMark.recdate = Date.now();
-        newMark.confiscation = {};
-        newMark.confiscation.code = 1;
-        newMark.confiscation.name = 'Лишение';
-        newMark.confiscation.note = '';
-        setNewMark(newMark);
+        newData[e.currentTarget.dataset.id] = e.currentTarget.value;
+        newData.recdate = Date.now();
+        newData.confiscation = {};
+        newData.confiscation.code = 1;
+        newData.confiscation.name = 'Лишение';
+        newData.confiscation.note = '';
+        setNewData(newData);
+        if(saveKey) handleErrors();
         break;
       case 'certificateWithdrawal':
         if (e.currentTarget.dataset.id !== 'name') {
-          newMark[e.currentTarget.dataset.id] = e.currentTarget.value;
-          newMark.recdate = Date.now();
-          newMark.confDateEnd = '2020-12-12'; //Тестовое значение, уточнить необходимость ввода данных
-          newMark.confDocNum = '-'; //Тестовое значение, уточнить необходимость ввода данных
-          newMark.confiscation = {};
-          newMark.confiscation.code = 2;
-          newMark.confiscation.name = 'Изъятие';
-          newMark.confiscation.note = '';
+          newData[e.currentTarget.dataset.id] = e.currentTarget.value;
+          newData.recdate = Date.now();
+          newData.confDateEnd = '2020-12-12'; //Тестовое значение, уточнить необходимость ввода данных
+          newData.confDocNum = '-'; //Тестовое значение, уточнить необходимость ввода данных
+          newData.confiscation = {};
+          newData.confiscation.code = 2;
+          newData.confiscation.name = 'Изъятие';
+          newData.confiscation.note = '';
         } else {
-          newMark.userid = usersLib.find((item) => item.userid == e.target.value);
+          newData.userid = usersLib.find((item) => item.userid == e.target.value);
         }
-        setNewMark(newMark);
+        setNewData(newData);
+        if(saveKey) handleErrors();
         break;
       case 'boatDrivingLicenseSpecmarksList':
-        newMark.markDate = `${new Date().toISOString().slice(0, 10)} ${new Date()
+        newData.markDate = `${new Date().toISOString().slice(0, 10)} ${new Date()
           .toISOString()
           .slice(11, 23)}`;
-        newMark[e.currentTarget.dataset.id] = e.currentTarget.value;
-        setNewMark(structuredClone(newMark));
+        newData[e.currentTarget.dataset.id] = e.currentTarget.value;
+        setNewData(structuredClone(newData));
+        if(saveKey) handleErrors();
         break;
       default:
       break;
     }
-
-    setNewMark(structuredClone(newMark));
+    setNewData(structuredClone(newData));
   };
 
   const handleSave = () => {
-    switch (modalWindowInputs.keyTable) {
-      case 'lossControl':
-      dispatch(addNewConfMark(newMark, licenseIdModal));
-      break;
-      case 'certificateWithdrawal':
-      dispatch(addNewConfMark(newMark, licenseIdModal));
-      break;
-      case 'boatDrivingLicenseSpecmarksList':
-      newMark.markDate = `${new Date().toISOString().slice(0, 10)} ${new Date()
-        .toISOString()
-        .slice(11, 23)}`;
-      newMark.licenseId = licenseIdModal;
-      setNewMark(structuredClone(newMark));
-      if (newMark.mark !== '') {
-        dispatch(addNewSpecialMark(newMark));
+    if (!handleErrors()){
+      switch (modalWindowInputs.keyTable) {
+        case 'lossControl':
+          dispatch(addNewConfMark(newData, licenseIdModal));
+          break;
+        case 'certificateWithdrawal':
+          dispatch(addNewConfMark(newData, licenseIdModal));
+          break;
+        case 'boatDrivingLicenseSpecmarksList':
+        newData.markDate = `${new Date().toISOString().slice(0, 10)} ${new Date()
+          .toISOString()
+          .slice(11, 23)}`;
+        newData.licenseId = licenseIdModal;
+        setNewData(structuredClone(newData));
+        if (newData.mark !== '') {
+          dispatch(addNewSpecialMark(newData));
+        }
+        break;
+        default:
+        setShowModal(false);
+        setNewData({});
+        break;
       }
-      break;
-      default:
       setShowModal(false);
-      setNewMark({});
-      break;
+      setNewData({});
+    } else {
+      setSaveKey(true)
     }
-    setShowModal(false);
-    setNewMark({});
     };
 
+  const handleErrors = () => {
+    let newErrors = {}
+    switch (modalWindowInputs.keyTable) {
+      case "certificateWithdrawal":
+        errorsFields.forEach((elem) => {
+          if (!newData[elem] || newData[elem] === "") {
+            if (elem !== "name" && elem !== "userPositions") {
+              newErrors[elem] = "Заполните поле"
+            }
+          }
+        })
+        break;
+      default:
+        errorsFields.forEach((elem) => {
+          if (!newData[elem] || newData[elem] === "") {
+            newErrors[elem] = "Заполните поле"
+          }
+        })
+        break;
+    }
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+      return true
+    } else {
+      setErrors({})
+      return false
+    }
+  }
+
   return (
-  <Modal
-    show={showModal}
-    onHide={() => {
-    setShowModal(false);
-    setNewMark({});
-    }}
-    size="lg">
-    <Modal.Header closeButton>
-    <Modal.Title>
-      {modalWindowInputs.caption}
-      {/* {options[type]} */}
-    </Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-    <Form>
-      {modalWindowInputs.nameColumn.map((item) => {
-      if (item.key === 'name') {
-        return (
-        <Form.Group className="mb-3">
-          <Form.Label>{item.value}</Form.Label>
-          <Form.Select
-          data-id={item.key}
-          type="select"
-          onChange={(e) => {
-            handleChange(e);
-          }}>
-          {usersLib.map((elem) => (
-            <option
+    <Modal
+      show={showModal}
+      onHide={() => {
+      setShowModal(false);
+      setNewData({});
+      }}
+      size="lg">
+      <Modal.Header closeButton>
+      <Modal.Title>
+        {modalWindowInputs.caption}
+        {/* {options[type]} */}
+      </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+      <Form>
+        {modalWindowInputs.nameColumn.map((item) => {
+        if (item.key === 'name') {
+          return (
+          <Form.Group className="mb-3">
+            <Form.Label>{item.value}</Form.Label>
+            <Form.Select
             data-id={item.key}
-            value={elem.userid}>
-            {elem.name}
-            </option>
-          ))}
-          </Form.Select>
-        </Form.Group>
-        );
-      }
-      if (
-        item.key !== 'markDate' &&
-        item.key !== 'userPositions' &&
-        (item.key === 'confDateEnd' || item.key === 'confDate')
-      ) {
-        return (
-        <Form.Group className="mb-3">
-          <Form.Label>{item.value}</Form.Label>
-          <Form.Control
-          data-id={item.key}
-          type="date"
-          onChange={(e) => {
-            handleChange(e);
-          }}
-          />
-        </Form.Group>
-        );
-      }
-      if (item.key === 'mark') {
-        return (
-        <Form.Group className="mb-3">
-          <Form.Label>{item.value}</Form.Label>
-          <Form.Control
-          data-id={item.key}
-          type="text"
-          value={!!newMark.mark ? newMark.mark  : ""}
-          onChange={(e) => {
-            handleChange(e);
-          }}
-          />
-        </Form.Group>
-        );
-      }
-      if (item.key !== 'markDate' && item.key !== 'userPositions') {
-        return (
-        <Form.Group className="mb-3">
-          <Form.Label>{item.value}</Form.Label>
-          <Form.Control
-          data-id={item.key}
-          type="text"
-          onChange={(e) => {
-            handleChange(e);
-          }}
-          />
-        </Form.Group>
-        );
-      }
-      })}
-    </Form>
-    </Modal.Body>
-    <Modal.Footer>
-    <Button
-      variant="secondary"
-      onClick={() => {
-        console.log("newMark", newMark)
-        setType(null);
-        setShowModal(false);
-        setDataForEdit(null);
-        setNewMark({});
-      }}>
-      Закрыть
-    </Button>
-    <Button
-      variant="primary"
-      onClick={handleSave}>
-      Сохранить
-    </Button>
-    </Modal.Footer>
-  </Modal>
+            type="select"
+            onChange={(e) => {
+              handleChange(e);
+            }}>
+            {usersLib.map((elem) => (
+              <option
+              data-id={item.key}
+              value={elem.userid}>
+              {elem.name}
+              </option>
+            ))}
+            </Form.Select>
+          </Form.Group>
+          );
+        }
+        if (
+          item.key !== 'markDate' &&
+          item.key !== 'userPositions' &&
+          (item.key === 'confDateEnd' || item.key === 'confDate')
+        ) {
+          return (
+          <Form.Group className="mb-3">
+            <Form.Label>{item.value}</Form.Label>
+            <Form.Control
+              isInvalid={!!errors[item.key]}
+              data-id={item.key}
+              type="date"
+              onChange={(e) => {
+                handleChange(e);
+              }}
+            />
+            <Form.Control.Feedback type={'invalid'}>{errors[item.key]}</Form.Control.Feedback>
+          </Form.Group>
+          );
+        }
+        if (item.key === 'mark') {
+          return (
+          <Form.Group className="mb-3">
+            <Form.Label>{item.value}</Form.Label>
+            <Form.Control
+            data-id={item.key}
+            type="text"
+            isInvalid={!!errors[item.key]}
+            value={!!newData.mark ? newData.mark  : ""}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            />
+            <Form.Control.Feedback type={'invalid'}>{errors[item.key]}</Form.Control.Feedback>
+          </Form.Group>
+          );
+        }
+        if (item.key !== 'markDate' && item.key !== 'userPositions') {
+          return (
+          <Form.Group className="mb-3">
+            <Form.Label>{item.value}</Form.Label>
+            <Form.Control
+            data-id={item.key}
+            type="text"
+            isInvalid={!!errors[item.key]}
+            onChange={(e) => {
+              handleChange(e);
+            }}
+            />
+            <Form.Control.Feedback type={'invalid'}>{errors[item.key]}</Form.Control.Feedback>
+          </Form.Group>
+          );
+        }
+        })}
+      </Form>
+      </Modal.Body>
+      <Modal.Footer>
+      <Button
+        variant="secondary"
+        onClick={() => {
+          handleErrors()
+          setType(null);
+          setShowModal(false);
+          setDataForEdit(null);
+          setNewData({});
+        }}>
+        Закрыть
+      </Button>
+      <Button
+        variant="primary"
+        onClick={handleSave}>
+        Сохранить
+      </Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
