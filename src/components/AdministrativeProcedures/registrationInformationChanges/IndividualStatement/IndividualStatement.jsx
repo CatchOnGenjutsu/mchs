6e98c31f4abcf76090460b215817f61126907b4f
ilya <izaluzhnyi@gmail.com>
2023-04-12 11:37:28 +0300

    start create component  IndividualStatement

--- src/components/AdministrativeProcedures/registrationInformationChanges/IndividualStatement/IndividualStatement.jsx
+++ src/components/AdministrativeProcedures/registrationInformationChanges/IndividualStatement/IndividualStatement.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import {Form} from 'react-bootstrap';
+import styles from './IndividualStatement.module.css'
 
 function IndividualStatement(props) {
+
     return (
-        <>
-        <h2>Заявление для физ.лиц</h2>
+        <div className='d-flex flex-column align-items-center'>
+            <h2>Заявление для физ.лиц</h2>
             <p>о государственной регистрации изменений сведений, подлежащих внесению в судовую книгу для маломерного судна, за исключением гребных лодок, байдарок и надувных судов грузоподъемностью менее 225 килограммов</p>
-        </>
+            <div className={styles['form-container']}>
+                <Form>
+                    <Form.Group controlId="regNum" className={styles['reg-number']}>
+                        <Form.Label>Регистрационный номер маломерного судна:</Form.Label>
+                        <Form.Control type="text" readOnly={true} disabled={true} />
+                    </Form.Group>
+                        <Form.Group controlId="regNum" className={styles['reg-number']}>
+                            <Form.Label>Субъект хозяйствования:</Form.Label>
+                            <Form.Control type="text" readOnly={true} disabled={true} />
+                        </Form.Group>
+                    <Form.Group controlId="regNum" className={styles['reg-number']}>
+                        <Form.Label>Номер судового билета:</Form.Label>
+                        <Form.Control type="text" readOnly={true} disabled={true} />
+                    </Form.Group>
+                    <Form.Group controlId="regNum" className={styles['reg-number']}>
+                        <Form.Label>Дата выдачи судового билета:g</Form.Label>
+                        <Form.Control type="text" readOnly={true} disabled={true} />
+                    </Form.Group>
+                </Form>
+            </div>
+
+        </div>
     );
 }
 
