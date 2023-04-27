296414da325ba9bae6264d4c7d021d3f6c4f8928
CatchOnGenjutsu <kozlov.dmitriy.dev@gmail.com>
2023-04-27 16:38:46 +0300

    added functionality for collecting application data in state redux

--- src/components/AdministrativeProcedures/commonComponents/InfoRepresentPerson/optionInfoRepresentPerson.js
+++ src/components/AdministrativeProcedures/commonComponents/InfoRepresentPerson/optionInfoRepresentPerson.js
@@ -2,137 +2,194 @@ import {
   MAIN_URL,
   PORT,
   API_GET_DICTIONARY_RAYON_FOR_OBL,
-  API_GET_DICTIONARY_GOROD_FOR_RAYON
-} from "../../../../constants/constants"
+  API_GET_DICTIONARY_GOROD_FOR_RAYON,
+} from "../../../../constants/constants";
 
 import {setRayon,setGorod} from '../utilities'
+
 export const optionInfoRepresentPersonSummary = {
-  agentSurname:{
+  agentSurname: {
     key: "agentSurname",
     value: "Фамилия",
-    type:'text'
+    type: "text",
   },
-  agentName:{
+  agentName: {
     key: "agentName",
     value: "Имя",
-    type:'text'
+    type: "text",
   },
   agentMidname: {
     key: "agentMidname",
     value: "Отчество",
-    type:'text'
+    type: "text",
   },
-  agentDocType:{
+  agentDocType: {
     key: "agentDocType",
     value: "Документ",
-    type:'select',
-    selectOption: [{value:1, label:'Паспорт'}, {value:2, label:'Вид на жительство'}]
+    type: "select",
+    selectOption: [
+      { value: 1, label: "Паспорт", key: "agentDocType" },
+      { value: 2, label: "Вид на жительство", key: "agentDocType" },
+    ],
   },
-  agentSerialOfPassport:{
+  agentSerialOfPassport: {
     key: "agentSerialOfPassport",
     value: "Серия",
-    type:'text'
+    type: "text",
   },
-  agentNumberOfPassport:{
+  agentNumberOfPassport: {
     key: "agentNumberOfPassport",
     value: "Номер",
-    type:'text'
+    type: "text",
   },
-  agentDocDate:{
+  agentDocDate: {
     key: "agentDocDate",
     value: "Дата выдачи:",
-    type:'date'
+    type: "date",
   },
-  agentDocDepartment:{
+  agentDocDepartment: {
     key: "agentDocDepartment",
     value: "Кем выдан",
-    type:'text'
+    type: "text",
   },
-  agentPersNum:{
+  agentPersNum: {
     key: "agentPersNum",
     value: "Идентификационный номер",
-    type:'text'
+    type: "text",
   },
-}
+};
 
 export const optionInfoRepresentPersonAddress = {
-  agentCountry:{
+  agentCountry: {
     key: "agentCountry",
     value: "Страна",
-    type:'text'
+    type: "text",
+    readOnly: true,
+    defaultValue: "Республика Беларусь"
   },
-  agentOblId:{
+  agentOblId: {
     key: "agentOblId",
     value: "Область",
-    type:'select',
+    type: "select",
+    isDisabled: false,
+    isSearchable: false,
     selectOption: [
-      {value:1,label:"Брестская", key: "agentObl" },
-      {value:2,label:"Витебская", key: "agentObl"},
-      {value:3,label:"Гомельская", key: "agentObl"},
-      {value:4,label:"Гродненская", key: "agentObl"},
-      {value:6,label:"Минская", key: "agentObl"},
-      {value:7,label:"Могилевская", key: "agentObl"}
-    ]
+      { value: 1, label: "Брестская", key: "agentOblId" },
+      { value: 2, label: "Витебская", key: "agentOblId" },
+      { value: 3, label: "Гомельская", key: "agentOblId" },
+      { value: 4, label: "Гродненская", key: "agentOblId" },
+      { value: 6, label: "Минская", key: "agentOblId" },
+      { value: 7, label: "Могилевская", key: "agentOblId" },
+    ],
   },
   agentRayonId: {
     key: "agentRayonId",
     value: "Район",
-    type:'customSelect',
-    selectOption:[]
+    type: "select",
+    isSearchable: true,
+    disabled: true,
+    selectOption: [],
   },
   agentGorodId: {
     key: "agentGorodId",
     value: "Город",
-    type:'customSelect',
-    selectOption:[{value:0, label:'Все'}]
+    type: "select",
+    isSearchable: true,
+    disabled: true,
+    selectOption: [],
   },
-  agentUlica:{
+  agentUlica: {
     key: "agentUlica",
     value: "Улица",
-    type:'text'
+    type: "text",
+    readOnly: false
   },
   agentDom: {
     key: "agentDom",
     value: "Дом",
-    type:'text'
+    type: "text",
+    readOnly: false
   },
   agentKorpus: {
     key: "agentKorpus",
     value: "Корпус",
-    type:'text'
+    type: "text",
+    readOnly: false
   },
-  agentKv:{
+  agentKv: {
     key: "agentKv",
     value: "Квартира",
-    type:'text'
+    type: "text",
+    readOnly: false
   },
-  agentPhone:{
+  agentPhone: {
     key: "agentPhone",
     value: "Телефон",
-    type:'text'
+    type: "text",
+    readOnly: false
   },
-}
+};
 
 export const powerOfAttorney = {
   key: "powerOfAttorney",
   value: "Сведения о доверенности",
-  type:'text'
-}
+  type: "text",
+  readOnly: false
+};
 
 export const setOptionsRayonForOblast = async (id) => {
-      const response = await fetch(MAIN_URL + PORT + API_GET_DICTIONARY_RAYON_FOR_OBL + id);
-      const result = await response.json();
-      optionInfoRepresentPersonAddress.agentRayonId.selectOption.length = 0
-      result.forEach(item => {
-        optionInfoRepresentPersonAddress.agentRayonId.selectOption.push({value: item.id, label: item.name, key: "rayon"})
-      });
-}
+  const response = await fetch(
+    MAIN_URL + PORT + API_GET_DICTIONARY_RAYON_FOR_OBL + id,
+  );
+  const result = await response.json();
+  console.log("agentOblId", result)
+  optionInfoRepresentPersonAddress.agentRayonId.selectOption.length = 0;
+  result.forEach((item) => {
+    optionInfoRepresentPersonAddress.agentRayonId.selectOption.push({
+      value: item.id,
+      label: item.name,
+      key: "agentRayonId",
+    });
+  });
+  optionInfoRepresentPersonAddress.agentRayonId.disabled = false;
+  optionInfoRepresentPersonAddress.agentGorodId.disabled = true;
+  optionInfoRepresentPersonAddress.agentGorodId.selectOption = [];
+  // return optionInfoRepresentPersonAddress;
+};
 
+
+// export async function setOptions(id,key) {
+//   switch (key) {
+//       case "oblId":{
+//           fieldAddressOptions.rayonId.options = await setRayon(id, "optionsForInformationAboutIndividual")
+//           fieldAddressOptions.rayonId.disabled=false
+//           fieldAddressOptions.gorod_id.options = []
+//           fieldAddressOptions.gorod_id.disabled = true
+//           return fieldAddressOptions
+//       }
+//       case "rayonId":{
+//           fieldAddressOptions.gorod_id.options = await  setGorod(id, "optionsForInformationAboutIndividuals")
+//           fieldAddressOptions.gorod_id.disabled = false
+//           return fieldAddressOptions
+//       }
+//       default: return fieldAddressOptions
+
+//   }
+
+// }
 export const setOptionsGorodForRayon = async (id) => {
-      const response = await fetch(MAIN_URL + PORT + API_GET_DICTIONARY_GOROD_FOR_RAYON + id);
-      const result = await response.json();
-      optionInfoRepresentPersonAddress.agentGorodId.selectOption.length = 0
-      result.forEach(item => {
-        optionInfoRepresentPersonAddress.agentGorodId.selectOption.push({value: item.id, label: item.name2, key: "gorod"})
-      });
-}
+  const response = await fetch(
+    MAIN_URL + PORT + API_GET_DICTIONARY_GOROD_FOR_RAYON + id,
+  );
+  const result = await response.json();
+  optionInfoRepresentPersonAddress.agentGorodId.selectOption.length = 0;
+  result.forEach((item) => {
+    optionInfoRepresentPersonAddress.agentGorodId.selectOption.push({
+      value: item.id,
+      label: item.name2,
+      key: "agentGorodId",
+    });
+  });
+  optionInfoRepresentPersonAddress.agentGorodId.disabled = false;
+
+};
---
+++ src/components/AdministrativeProcedures/commonComponents/InformationAboutBoat/InformationAboutBoat.jsx
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Form } from "react-bootstrap";
+import Select from "react-select";
+
+import { addNewStatementData } from "../../../../redux/statementReducer/actionsStatement";
+import {
+  setOptionsTypesBoat,
+  setOptionsVidBoat,
+  setOptionsBodyBoat,
+} from "../utilities";
+
+import styles from "./informationAboutBoat.module.css";
+import {
+  fieldBoatOptions,
+  setOptionsForBoat,
+} from "./optionsForInformationAboutBoat";
+
+function InformationAboutBoat() {
+  const [options, setOptions] = useState(fieldBoatOptions);
+  const dispatch = useDispatch();
+
+  const handleValue = async (e) => {
+    if (e) {
+      switch (true) {
+        case Object.keys(e).includes("target"):
+          dispatch(addNewStatementData({ [`${e.target.id}`]: e.target.value }));
+          break;
+        case Object.keys(e).includes("key"):
+          dispatch(addNewStatementData({ [`${e.key}`]: e.value }));
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const typesBoat = await setOptionsTypesBoat();
+      const kindsBoat = await setOptionsVidBoat();
+      const materialsBodyBoat = await setOptionsBodyBoat();
+      setOptions({
+        ...setOptionsForBoat(typesBoat, kindsBoat, materialsBodyBoat),
+      });
+    })();
+  }, []);
+  return (
+    <>
+      <h3>Сведения о маломерном судне</h3>
+      <div className={styles["container-information"]}>
+        <div className={styles["boat-information"]}>
+          {Object.values(options).map((option) => {
+            if (option.type === "text" || option.type === "date") {
+              return (
+                <Form.Group
+                  controlId={`${option.key}`}
+                  className={`${styles["common"]} ${
+                    styles[`box-${option.key}`]
+                  }`}>
+                  <Form.Label>{option.value}</Form.Label>
+                  <Form.Control
+                    onBlur={(e) => handleValue(e)}
+                    type={option.type}
+                    defaultValue={option.defaultValue}
+                    readOnly={option.readOnly}
+                  />
+                </Form.Group>
+              );
+            } else if (option.type === "selectSearch") {
+              return (
+                <Form.Group
+                  className={`${styles["common"]} ${
+                    styles[`box-${option.key}`]
+                  }`}>
+                  <Form.Label>{option.value}</Form.Label>
+                  <Select
+                    onChange={(e) => handleValue(e)}
+                    defaultValue={option.defaultValue}
+                    className={`${styles["selectSearch"]}`}
+                    classNamePrefix="select"
+                    placeholder="Выберите"
+                    id={option.key}
+                    isSearchable={false}
+                    name={option.key}
+                    options={option.options}
+                  />
+                </Form.Group>
+              );
+            }
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default InformationAboutBoat;
