42b7a7949be7e347b57d829c8131d784836985bc
CatchOnGenjutsu <kozlov.dmitriy.dev@gmail.com>
2023-05-04 09:54:55 +0300

    fix opening registration change component

--- src/components/AdministrativeProcedures/registrationInformationChanges/IndividualStatement/IndividualStatement.jsx
+++ src/components/AdministrativeProcedures/registrationInformationChanges/IndividualStatement/IndividualStatement.jsx
@@ -1,37 +1,66 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { Form } from "react-bootstrap";
-import { ProgressBar } from  'react-loader-spinner'
+import { ProgressBar } from "react-loader-spinner";
 import styles from "./IndividualStatement.module.css";
 import InformationAboutIndividual from "../../commonComponents/InformationAboutIndividual/InformationAboutIndividual";
 import InfoRepresentPerson from "../../commonComponents/InfoRepresentPerson/InfoRepresentPerson";
 import TableAppBoatReg from "../../commonComponents/TablesAppBoatReg/TableAppBoatReg";
 import InformationAboutBoat from "../../commonComponents/InformationAboutBoat/InformationAboutBoat";
 import AppFooter from "../../commonComponents/AppFooter/AppFooter";
-import OtherInformation from "../../commonComponents/OtherInformation/OtherInformation"
+import OtherInformation from "../../commonComponents/OtherInformation/OtherInformation";
 import {
-    optionSelectChangeType,
-    boatCardAppEngDtoList,
-    boatCardAppSmDtoList,
-    boatCardAppDealsDtoList,
-    readStatusForInputField
+  optionSelectChangeType,
+  boatCardAppEngDtoList,
+  boatCardAppSmDtoList,
+  boatCardAppDealsDtoList,
+  readStatusForInputField,
 } from "./optionsForIndividualStatement";
 import {
-    MAIN_URL,
-    PORT,
-    API_GET_BOAT_INFO_CARD,
-} from "../../../../constants/constants";
-
+  fieldAddressOptions,
+  fieldPassportOptions,
+} from "../../commonComponents/InformationAboutIndividual/optionsForInformationAboutIndividual";
+import { fieldBoatOptions } from "../../commonComponents/InformationAboutBoat/optionsForInformationAboutBoat";
+import { MAIN_URL, PORT, API_GET_BOAT_INFO_CARD } from "../../../../constants/constants";
 
 function IndividualStatement() {
   const location = useLocation();
   const { idBoadCard } = location.state || {};
-  const [idTypeStatement,setIdTypeStatement] = useState('1')
+  const [idTypeStatement, setIdTypeStatement] = useState("1");
   const [data, setData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
 
+  const [newData, setNewData] = useState({});
+  const [errors, setErrors] = useState({});
+  const [saveKey, setSaveKey] = useState(false);
+
+  const errorsFields = [];
+  Object.entries(fieldAddressOptions).map((item) => (item[1].required ? errorsFields.push(item[0]) : null));
+  Object.entries(fieldPassportOptions).map((item) => (item[1].required ? errorsFields.push(item[0]) : null));
+  Object.entries(fieldBoatOptions).map((item) => (item[1].required ? errorsFields.push(item[0]) : null));
+
+  const handleErrors = () => {
+    let newErrors = {};
+    errorsFields.forEach((elem) => {
+      if (!newData[elem] || newData[elem] === "") {
+        newErrors[elem] = "Заполните поле";
+      }
+    });
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+      return true;
+    } else {
+      setErrors({});
+      return false;
+    }
+  };
+
+  const updateNewData = (key, value) => {
+    newData[key] = value;
+  };
+
   useEffect(() => {
-      async function fetchData() {
+    async function fetchData() {
       const response = await fetch(MAIN_URL + PORT + API_GET_BOAT_INFO_CARD + String(idBoadCard));
       const data = await response.json();
       setData(data);
@@ -39,40 +68,38 @@ function IndividualStatement() {
     }
     fetchData();
   }, []);
-  console.log(data)
-   if(isLoading){
-       return (
-           <div className={`d-flex flex-column align-items-center `}>
-           <ProgressBar
-               height="80"
-               width="80"
-               ariaLabel="progress-bar-loading"
-               wrapperStyle={{}}
-               wrapperClass="progress-bar-wrapper"
-               borderColor = '#F4442E'
-               barColor = '#51E5FF'
-           />
-           </div>
-       )
-   }
+
+  console.log(data);
+  if (isLoading) {
+    return (
+      <div className={`d-flex flex-column align-items-center `}>
+        <ProgressBar
+          height="80"
+          width="80"
+          ariaLabel="progress-bar-loading"
+          wrapperStyle={{}}
+          wrapperClass="progress-bar-wrapper"
+          borderColor="#F4442E"
+          barColor="#51E5FF"
+        />
+      </div>
+    );
+  }
 
   return (
     <div className={`d-flex flex-column align-items-center justify-content-center`}>
       <h2>Заявление для физ.лиц</h2>
       <p>
-        о государственной регистрации изменений сведений, подлежащих внесению в
-        судовую книгу для маломерного судна, за исключением гребных лодок,
-        байдарок и надувных судов грузоподъемностью менее 225 килограммов
+        о государственной регистрации изменений сведений, подлежащих внесению в судовую книгу для маломерного
+        судна, за исключением гребных лодок, байдарок и надувных судов грузоподъемностью менее 225 килограммов
       </p>
       <div className={styles["form-container"]}>
         <Form>
           <Form.Group className={styles["header"]}>
             <Form.Label>Какие изменения вносятся:</Form.Label>
-            <Form.Select
-            onChange={(e)=>setIdTypeStatement(e.currentTarget.value)}
-            >
+            <Form.Select onChange={(e) => setIdTypeStatement(e.currentTarget.value)}>
               {optionSelectChangeType.map((el) => (
-                  <option value={el.id}>{el.value}</option>
+                <option value={el.id}>{el.value}</option>
               ))}
             </Form.Select>
           </Form.Group>
@@ -113,38 +140,46 @@ function IndividualStatement() {
               disabled={true}
             />
           </Form.Group>
-          <InformationAboutIndividual data={data} />
+          <InformationAboutIndividual
+            data={data}
+            updateNewData={updateNewData}
+            saveKey={saveKey}
+            handleErrors={handleErrors}
+            errors={errors}
+          />
           <InfoRepresentPerson />
-          {!(idTypeStatement==='1')?(
-              <InformationAboutBoat
-                  fieldStatus={readStatusForInputField}
-              />
-          ):''}
-          {idTypeStatement==='4'?<OtherInformation/>:''}
+          {!(idTypeStatement === "1") ? <InformationAboutBoat fieldStatus={readStatusForInputField} /> : ""}
+          {idTypeStatement === "4" ? <OtherInformation /> : ""}
 
-          <Form.Group >
-            <Form.Label><h3>Основание для внесения изменений</h3></Form.Label>
+          <Form.Group>
+            <Form.Label>
+              <h3>Основание для внесения изменений</h3>
+            </Form.Label>
             <Form.Control
-                id="appReason"
-                type="text"
+              id="appReason"
+              type="text"
             />
           </Form.Group>
-          {(idTypeStatement==='2'||idTypeStatement==='3')?(
-              <TableAppBoatReg
-                  tableOptions={boatCardAppEngDtoList}
-                  dataEngines={data.enginesList}
-              />
-          ):''}
+          {idTypeStatement === "2" || idTypeStatement === "3" ? (
+            <TableAppBoatReg
+              tableOptions={boatCardAppEngDtoList}
+              dataEngines={data.enginesList}
+            />
+          ) : (
+            ""
+          )}
 
-          {idTypeStatement==='1'?(
-              <TableAppBoatReg
-                  tableOptions={boatCardAppDealsDtoList}
-                  // data={boatCardAppEngList}
-              />
-          ):''}
-          <TableAppBoatReg
-              tableOptions={boatCardAppSmDtoList}
+          {idTypeStatement === "1" ? (
+            <TableAppBoatReg
+              tableOptions={boatCardAppDealsDtoList}
               // data={boatCardAppEngList}
+            />
+          ) : (
+            ""
+          )}
+          <TableAppBoatReg
+            tableOptions={boatCardAppSmDtoList}
+            // data={boatCardAppEngList}
           />
           <AppFooter />
         </Form>
