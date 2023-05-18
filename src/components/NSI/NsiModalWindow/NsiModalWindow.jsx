a38ab09cc8d04faa007b63a7155851a3f23ea792
CatchOnGenjutsu <kozlov.dmitriy.dev@gmail.com>
2023-05-18 17:47:18 +0300

    fix some bugs

--- src/components/NSI/NsiModalWindow/NsiModalWindow.jsx
+++ src/components/NSI/NsiModalWindow/NsiModalWindow.jsx
@@ -179,6 +179,7 @@ export default function NsiModalWindow({
                   method: "POST",
                   body: formData,
                 });
+                break;
               case window.location.pathname.includes("paidproc"):
                 request = await fetch(MAIN_URL + PORT + API_EDIT_PAID_PROC_INFO + id, {
                   method: "POST",
