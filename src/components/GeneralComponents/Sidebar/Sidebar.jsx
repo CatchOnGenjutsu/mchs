7666f8766cc873c5f7b4526f8313ccc29e899871
CatchOnGenjutsu <kozlov.dmitriy.dev@gmail.com>
2023-07-18 17:54:38 +0300

    added new sidebar component

--- src/components/GeneralComponents/Sidebar/Sidebar.jsx
+++ src/components/GeneralComponents/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 import { sidebarSettings } from "./sidebarSettings";
 import { clearNewStatement } from "../../../redux/statementReducer/actionsStatement";
@@ -9,113 +9,104 @@ import { v4 as uuidv4 } from "uuid";
 
 import styles from "./Sidebar.module.css";
 
+import {
+  Sidebar as SidebarCustom,
+  sidebarClasses,
+  Menu,
+  MenuItem,
+  SubMenu,
+  menuClasses,
+} from "react-pro-sidebar";
+
 export default function Sidebar() {
-  const [sidebarListArray, setSidebarListArray] = useState(sidebarSettings);
   const [isLogin, setIsLogin] = useState("");
-  const dispatch = useDispatch();
-
-  const showDropdownMenu = (e) => {
-    setSidebarListArray(
-      Object.values(sidebarListArray).map((item) => {
-        if (item.id === e.currentTarget.dataset.title && item.listModal !== undefined) {
-          item.isHidden = !item.isHidden;
-        } else if (e.currentTarget.dataset.title === "undefined" && item.listModal !== undefined) {
-          item.isHidden = true;
-        }
-        return item;
-      }),
-    );
-  };
-
-  const handleMenuItemClick = () => {
-    dispatch(clearNewStatement());
-    setSidebarListArray(
-      Object.values(sidebarListArray).map((item) => {
-        const index = item.listModal.findIndex((elem) => elem.id === window.location.pathname.split("/")[1]);
-        if (index >= 0) {
-          item.isHidden = false;
-        } else {
-          item.isHidden = true;
-        }
-        return item;
-      }),
-    );
-  };
 
   useEffect(() => {
     window.location.href.includes("login") ? setIsLogin("login") : setIsLogin("");
-    setSidebarListArray(
-      Object.values(sidebarListArray).map((item) => {
-        const index = item.listModal.findIndex((elem) => elem.id === window.location.pathname.split("/")[1]);
-        if (index >= 0) {
-          item.isHidden = false;
-        } else {
-          item.isHidden = true;
-        }
-        return item;
-      }),
-    );
   }, [window.location.pathname]);
-  if (!isLogin) {
-    return (
-      <div className={styles["sidebar-container"]}>
-        <ul className={styles["sidebar-list"]}>
-          {Object.values(sidebarListArray).map((item) => (
-            <>
-              {item.listModal.length !== 0 ? (
-                <>
-                  <li
-                    onClick={showDropdownMenu}
-                    className={styles["sidebar-list-item"]}
-                    data-title={item.id}
-                    key={uuidv4()}>
-                    {item.title}
-                    {item.listModal.length !== 0 && (
-                      <img
-                        alt="arrow icon"
-                        className={!item.isHidden ? styles["rotated-image"] : ""}
-                        src="/assets/icon-down-arrow.png"></img>
-                    )}
-                  </li>
-                  <ul
-                    key={uuidv4()}
-                    className={`${styles["sidebar-list-modal"]} ${item.isHidden ? styles["hide"] : ""}`}>
-                    {item.listModal !== undefined
-                      ? item.listModal.map((elem) => (
-                          <NavLink
-                            onClick={() => handleMenuItemClick()}
-                            className={({ isActive }) =>
-                              isActive
-                                ? `${styles["sidebar-list-item-modal"]} ${styles["colored"]}`
-                                : `${styles["sidebar-list-item-modal"]} `
-                            }
-                            to={`/${elem.id}`}
-                            key={uuidv4()}
-                            data-id={elem.id}>
-                            {elem.title}
-                          </NavLink>
-                        ))
-                      : null}
-                  </ul>
-                </>
-              ) : (
-                <NavLink
-                  onClick={() => handleMenuItemClick()}
-                  className={({ isActive }) =>
-                    isActive
-                      ? `${styles["sidebar-nolist-item"]} ${styles["colored"]}`
-                      : `${styles["sidebar-nolist-item"]}`
-                  }
-                  to={`/${item.id}`}
-                  key={uuidv4()}
-                  id={item.id}>
-                  {item.title}
-                </NavLink>
-              )}
-            </>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+
+  const themeSetting = {
+    menu: {
+      menuContent: "rgb(57, 68, 77)",
+      icon: "#59d0ff",
+      hover: {
+        backgroundColor: "rgb(161, 170, 187)",
+        color: "rgb(0, 0, 0)",
+      },
+    },
+  };
+  const menuItemStyles = {
+    SubMenuExpandIcon: {
+      color: "white",
+      "&:hover": {
+        color: "rgb(0, 0, 0)",
+      },
+    },
+    subMenuContent: ({ level }) => ({
+      backgroundColor: level === 0 ? themeSetting.menu.menuContent : "transparent",
+    }),
+    button: {
+      "&:hover": {
+        backgroundColor: themeSetting.menu.hover.backgroundColor,
+        color: themeSetting.menu.hover.color,
+      },
+      [`&.active`]: {
+        backgroundColor: "rgb(0, 0, 0)",
+        color: "white",
+      },
+    },
+    label: ({ open }) => ({
+      fontWeight: open ? 600 : undefined,
+    }),
+  };
+  return (
+    <SidebarCustom
+      rootStyles={{
+        [`.${sidebarClasses.container}`]: {
+          position: "fixed",
+          top: "10vh",
+          height: "90vh",
+          width: "16vw",
+          paddingTop: "1vh",
+          color: "white",
+          userSelect: "none",
+        },
+      }}
+      backgroundColor="rgba(72, 86, 97, 255)"
+      hidden={isLogin === "login" ? true : false}>
+      <Menu menuItemStyles={menuItemStyles}>
+        {Object.values(sidebarSettings).map((item) => {
+          if (item.listModal.length > 0) {
+            return (
+              <SubMenu label={item.title}>
+                {item.listModal.map((elem) => {
+                  return (
+                    <NavLink
+                      className={({ isActive }) =>
+                        isActive
+                          ? `${styles.sidebar_navlink} ${styles.colored}`
+                          : `${styles.sidebar_navlink} `
+                      }
+                      to={`/${elem.id}`}>
+                      <MenuItem>{elem.title}</MenuItem>
+                    </NavLink>
+                  );
+                })}
+              </SubMenu>
+            );
+          } else {
+            return (
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? `${styles.sidebar_navlink} ${styles.colored}` : `${styles.sidebar_navlink} `
+                }
+                to={`/${item.id}`}>
+                <MenuItem className={styles.sidebar_menuitem}>{item.title}</MenuItem>
+              </NavLink>
+            );
+          }
+        })}
+      </Menu>
+    </SidebarCustom>
+  );
 }
